347f326b72f5d647d8c2792976e91c1aff4e7e88
= <=>
2023-05-09 14:24:48 +0700

    remove log

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
-import {
-  EMAIL_SENDER,
-  KEY_SENDER,
-  SES_AWS_SMTP_ENDPOINT,
-  SES_AWS_SMTP_PASWORD,
-  SES_AWS_SMTP_PORT,
-  SES_AWS_SMTP_USERNAME,
-} from 'src/constants';
+import { EMAIL_SENDER, KEY_SENDER } from 'src/constants';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
