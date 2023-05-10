99c59f677ba67518afb05bfb8bfe0ee7d1a9c2a5
= <=>
2023-05-10 13:57:37 +0700

    fix: rename controller

--- src/modules/favourite/favorite.controller.ts
+++ src/modules/favourite/favorite.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Body,
-  Param,
-  UsePipes,
-  ValidationPipe,
-  Post,
-  BadRequestException,
-} from '@nestjs/common';
+import { Controller, Body, Post, BadRequestException } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { AddFavoriteDto } from './dto';
 import { query } from 'express';
@@ -18,7 +8,7 @@ export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
 
   @Post('/')
-  async addLoctionToFavorite(@Body() bodyData: AddFavoriteDto) {
+  async addToFavorite(@Body() bodyData: AddFavoriteDto) {
     const { locationId, itineraryId } = bodyData;
     if (!locationId && !itineraryId) throw new BadRequestException({ message: 'You must transport data', data: null });
 
