4c10a272a8b48b892e8dbd473359d8a9e8632a7d
= <=>
2023-05-10 13:56:57 +0700

    save

--- src/modules/favourite/favorite.controller.ts
+++ src/modules/favourite/favorite.controller.ts
@@ -1,28 +1,30 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Body,
-  Param,
-  UsePipes,
-  ValidationPipe,
-  Post,
-  BadRequestException,
-} from '@nestjs/common';
+import { Controller, Delete, Body, Post, BadRequestException } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { AddFavoriteDto } from './dto';
-import { query } from 'express';
 
 @Controller('/favorites')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
 
   @Post('/')
-  async addLoctionToFavorite(@Body() bodyData: AddFavoriteDto) {
+  async addToFavorite(@Body() bodyData: AddFavoriteDto) {
     const { locationId, itineraryId } = bodyData;
     if (!locationId && !itineraryId) throw new BadRequestException({ message: 'You must transport data', data: null });
 
     const favorite = await this.favoriteService.addToFavorite(bodyData);
+    if (!favorite) throw new BadRequestException({ message: "Don't request to server", data: null });
+    return {
+      mesage: 'Success',
+      data: favorite,
+    };
+  }
+
+  @Delete('/')
+  async removeToFavorite(@Body() bodyData: AddFavoriteDto) {
+    const { locationId, itineraryId } = bodyData;
+    if (!locationId && !itineraryId) throw new BadRequestException({ message: 'You must transport data', data: null });
+
+    const favorite = await this.favoriteService.removeLoctionToFavorite(bodyData);
     if (!favorite) throw new BadRequestException({ message: "Don't request to server", data: null });
     return {
       mesage: 'Success',
