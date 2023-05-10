58d063aac0545abbb0279199202aab5cf9894276
= <=>
2023-05-10 10:19:36 +0700

    save

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,36 +1,18 @@
-import { Controller, Get, Query, Param, NotFoundException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Query, Param, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { ObjectId } from 'mongoose';
 import { LocationQueryDto } from './dto';
-import { LocationType } from 'src/utils';
-import { LocationSchema } from 'src/schemas/locations';
 
 @Controller('/locations')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
-  @Get('/related')
-  async getListRelatedLocations(@Query('type') type: string, @Query('locationId') locationId: ObjectId) {
-    const locations = await this.locationService.getListRelatedLocations(type, locationId);
-
+  @Get('/:locationId')
+  async getDetailLocation(@Param('locationId') locationId: ObjectId) {
+    const locations = await this.locationService.getDetailLocation(locationId);
     return {
       mesage: 'Success',
       data: locations,
-    };
-  }
-
-  @Get('/:locationId')
-  async getDetailLocation(@Param('locationId') locationId: ObjectId) {
-    const location = await this.locationService.getDetailLocation(locationId);
-    if (!location) {
-      throw new NotFoundException({
-        message: 'Location is not existed',
-        data: null,
-      });
-    }
-    return {
-      mesage: 'Success',
-      data: location,
     };
   }
 
